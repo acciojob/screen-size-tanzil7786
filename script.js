d863f187a974aa608dd740c03409cad0ff0de022
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-04 12:40:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 //your JS code here. If required.
 
-const expectedWidth=window.innerWidth;
-const expectedHeight=window.innerHeight;
-const sizedElement = document.createElement("h1");
-sizedElement.innerText =`Width : ${expectedWidth} and Height: ${expectedHeight}`;
-document.body.append(sizedElement);
 
+// const sizedElement = document.createElement("h1");
+// sizedElement.innerText =`Width : ${expectedWidth} and Height: ${expectedHeight}`;
+// document.body.append(sizedElement);
+
+function getSize(){
+	var expectedWidth=document.documentElement.clientWidth;
+	var expectedHeight=document.documentElement.clientHeight;
+	document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + expectedWidth + " and Height: " + expectedHeight + "</h1>";
+
+}
 
 
 

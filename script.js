d84210c6427edcf666130305478f53fac1c9acca
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-04 12:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const sizedElement = document.createElement("h1");
+sizedElement.innerText =`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+document.body.append(sizedElement);
+
+window.onresize = function(){
+	sizedElement.innerText =`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+}
